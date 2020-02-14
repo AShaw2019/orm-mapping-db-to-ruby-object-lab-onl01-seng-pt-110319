f54a1ce04cda6431401cf67153de5e642bfaf66b
Ashley Twomey <aprier091012@gmail.com>
2020-02-14 02:28:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,8 +33,8 @@ class Student
   
   def save
     sql = <<-SQL
-       sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
